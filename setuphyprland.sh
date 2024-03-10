fc872c8fd5b7c0070746b64b9bc9cd6de33feb38
LucaDangeloS <lucad7@gmail.com>
2024-03-10 19:55:21 +0100

    Fast commit

--- setuphyprland.sh
+++ setuphyprland.sh
@@ -1,4 +1,4 @@
-yay -S hyprland-git dunst xdg-desktop-portal-hyprland polkit-kde-agent rofi-lbonn-wayland-git waybar hyprlock hyprpicker-git
+yay -S hyprland-git dunst xdg-desktop-portal-hyprland polkit-kde-agent rofi-lbonn-wayland-git waybar hyprlock hyprpicker-git grimblast
 sudo pacman -S hyprpaper
 
 #sudo pacman -S --devel libgdk-3 libgtk-3 gtk-layer-shell libpango libgdk_pixbuf-2 libcairo libcairo-gobject libgio libglib-2 libgobject-2 libgcc glibc 
