a404ebb0eb3505b8295fbac6d7ebac016be8f066
LucaDangeloS <l.dangelo@udc.es>
2024-03-11 10:45:31 +0100

    Fast commit

--- setuphyprland.sh
+++ setuphyprland.sh
@@ -1,4 +1,6 @@
 yay -S hyprland-git dunst xdg-desktop-portal-hyprland polkit-kde-agent rofi-lbonn-wayland-git waybar hyprlock hyprpicker-git grimblast cliphist
 sudo pacman -S hyprpaper
 
+git clone https://github.com/lr-tech/rofi-themes-collection.git
+mkdir -p ~/.local/share/rofi/themes/
 #sudo pacman -S --devel libgdk-3 libgtk-3 gtk-layer-shell libpango libgdk_pixbuf-2 libcairo libcairo-gobject libgio libglib-2 libgobject-2 libgcc glibc 
